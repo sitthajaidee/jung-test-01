1f9173c457c57f7a01ac4e8a48b1717d7058875e
Sittha Jaidee <sitthaja@ais.co.th>
2023-06-16 17:15:39 +0700

    change return on axiostrigger function 03

--- handler/queryproduct.js
+++ handler/queryproduct.js
@@ -1,11 +1,11 @@
 module.exports.queryproduct = async (event) => {
-  const inputFromAxiostrigger = event.selectedTitle;
+  const inputFromAxiostrigger = event;
   console.log(JSON.stringify(inputFromAxiostrigger));
 
   return {
     // statusCode: 200,
     body: JSON.stringify({
-      message: "This is input from axiostrigger " + inputFromAxiostrigger,
+      message: "This is input from axiostrigger 03 " + inputFromAxiostrigger,
     }),
   };
 };
