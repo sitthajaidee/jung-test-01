9ee06978301a4d22c76281236c6e255420019161
Sittha Jaidee <sitthaja@ais.co.th>
2023-06-19 12:53:51 +0700

    test velocity template

--- handler/axiostrigger.js
+++ handler/axiostrigger.js
@@ -1,32 +1,8 @@
-const axios = require("axios");
-
-const url = "https://dummyjson.com/products";
-
 module.exports.axiostrigger = (event, context, callback) => {
-  // try {
-  //   const response = await axios.get(url);
-  //   const responseData = response.data;
-  //   const title = responseData.products.map((product) => product.title);
-  //   // console.log("Success to get product from API");
+  const response = {
+    statusCode: 200,
+    body: "TEST " + event,
+  };
 
-  //   // Receive query param in URL like dev/axiostrigger?n=2
-  //   const n = event.queryStringParameters && event.queryStringParameters.n;
-  //   const selectedTitle = title[n];
-  //   const responseBody = { axiostriggerResult: selectedTitle };
-  //   console.log("event = ", event);
-  //   console.log("selectedTitle = ", selectedTitle);
-  //   console.log("Query-string = ", n);
-  //   console.log("selectedTitle =", responseBody);
-  //   return responseBody;
-  // } catch (error) {
-  //   console.error(error);
-  //   return {
-  //     statusCode: 503,
-  //     body: JSON.stringify({
-  //       message: "Web service unavailable",
-  //     }),
-  //   };
-  // }
-  console.log("event = ", event);
-  callback(null, null);
+  callback(null, response);
 };
