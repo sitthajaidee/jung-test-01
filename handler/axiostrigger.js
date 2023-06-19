751eb5cd5d959f0d7bbcbc1cae013cbce4900738
Sittha Jaidee <sitthaja@ais.co.th>
2023-06-19 13:08:59 +0700

    test velocity template

--- handler/axiostrigger.js
+++ handler/axiostrigger.js
@@ -2,36 +2,29 @@ const axios = require("axios");
 
 const url = "https://dummyjson.com/products";
 
-module.exports.axiostrigger = async (event) => {
-  // try {
-  //   const response = await axios.get(url);
-  //   const responseData = response.data;
-  //   const title = responseData.products.map((product) => product.title);
-  //   // console.log("Success to get product from API");
+module.exports.axiostrigger = async (event, context, callback) => {
+  try {
+    const response = await axios.get(url);
+    const responseData = response.data;
+    const title = responseData.products.map((product) => product.title);
+    // console.log("Success to get product from API");
 
-  //   // Receive query param in URL like dev/axiostrigger?n=2
-  //   const n = event.queryStringParameters && event.queryStringParameters.n;
-  //   const selectedTitle = title[n];
-  //   const responseBody = { axiostriggerResult: selectedTitle };
-  //   console.log("event = ", event);
-  //   console.log("selectedTitle = ", selectedTitle);
-  //   console.log("Query-string = ", n);
-  //   console.log("selectedTitle =", responseBody);
-  //   return responseBody;
-  // } catch (error) {
-  //   console.error(error);
-  //   return {
-  //     statusCode: 503,
-  //     body: JSON.stringify({
-  //       message: "Web service unavailable",
-  //     }),
-  //   };
-  // }
-  console.log("event = ", event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: event,
-    }),
-  };
+    // Receive query param in URL like dev/axiostrigger?n=2
+    const n = event.queryStringParameters && event.queryStringParameters.n;
+    const selectedTitle = title[n];
+    const responseBody = { axiostriggerResult: selectedTitle };
+    console.log("event = ", event);
+    console.log("selectedTitle = ", selectedTitle);
+    console.log("Query-string = ", n);
+    console.log("selectedTitle =", responseBody);
+    return responseBody;
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 503,
+      body: JSON.stringify({
+        message: "Web service unavailable",
+      }),
+    };
+  }
 };
