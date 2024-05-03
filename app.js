16ede6fda5073bf0c134a372a44f6a33cf861d5e
Sittha Jaidee <sitthaja@ais.co.th>
2024-05-03 16:38:49 +0700

    update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const playbackRoutes = require("./routes/playbackRoutes");
 const proxyRoutes = require("./routes/proxyRoutes");
-const https = require("https");
-const fs = require("fs");
-const path = require("path");
+// const https = require("https");
+// const fs = require("fs");
+// const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -14,20 +14,6 @@ app.use(bodyParser.json());
 app.use(playbackRoutes);
 app.use(proxyRoutes);
 
-// Paths to SSL/TLS certificate and key
-const keyPath = path.join(__dirname, "certs", "localhost.key");
-const certPath = path.join(__dirname, "certs", "localhost.crt");
+app.listen(port, () => console.log("Listening on port " + port));
 
-// Read SSL/TLS certificate and key
-const options = {
-  key: fs.readFileSync(keyPath),
-  cert: fs.readFileSync(certPath),
-};
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-https.createServer(options, app).listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
-});
+module.exports = app;
