018dbab906a38d9634958c97e91f6c82c68b1d37
Sittha Jaidee <sitthaja@ais.co.th>
2024-05-03 16:36:20 +0700

    rollback app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const playbackRoutes = require("./routes/playbackRoutes");
 const proxyRoutes = require("./routes/proxyRoutes");
-// const https = require("https");
-// const fs = require("fs");
-// const path = require("path");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
-// const port = 3000;
+const port = 3000;
 
 app.use(bodyParser.json());
 
@@ -15,19 +15,19 @@ app.use(playbackRoutes);
 app.use(proxyRoutes);
 
 // Paths to SSL/TLS certificate and key
-// const keyPath = path.join(__dirname, "certs", "localhost.key");
-// const certPath = path.join(__dirname, "certs", "localhost.crt");
+const keyPath = path.join(__dirname, "certs", "localhost.key");
+const certPath = path.join(__dirname, "certs", "localhost.crt");
 
 // Read SSL/TLS certificate and key
-// const options = {
-//   key: fs.readFileSync(keyPath),
-//   cert: fs.readFileSync(certPath),
-// };
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath),
+};
 
 // app.listen(port, () => {
 //   console.log(`Server is running on http://localhost:${port}`);
 // });
 
-// https.createServer(options, app).listen(port, () => {
-//   console.log(`Server is running on https://localhost:${port}`);
-// });
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server is running on https://localhost:${port}`);
+});
