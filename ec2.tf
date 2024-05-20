8d1d17de9ab906318d86141b6496905d64bfb3cb
Sittha Jaidee <sitthaja@ais.co.th>
2024-05-20 11:40:09 +0700

    Fixing ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "jung-ec2" {
-  ami           = "ami-0ac9397cab55f5044"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.jung-subnet-a.id
-  vpc_security_group_ids = [aws_security_group.jung-sg.name]
-  iam_instance_profile = data.aws_iam_instance_profile.ssm_instance_profile.name
+  ami                    = "ami-0ac9397cab55f5044" # Ensure this is the correct AMI ID for your region and OS
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.jung-subnet-a.id
+  vpc_security_group_ids = [aws_security_group.jung-sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
     Name      = "jung-ec2"
