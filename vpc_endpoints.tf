e5b703e87eacb472bc2e44efb046367471e46b81
Sittha Jaidee <sitthaja@ais.co.th>
2024-05-20 13:44:01 +0700

    fix vpc_endpoint

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -1,8 +1,9 @@
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id       = aws_vpc.jung-main.id
-  service_name = "com.amazonaws.ap-southeast-1.ssm"
-  subnet_ids   = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
+  vpc_id            = aws_vpc.jung-main.id
+  service_name      = "com.amazonaws.ap-southeast-1.ssm"
+  vpc_endpoint_type = "Interface"
   security_group_ids = [aws_security_group.jung-sg.id]
+  subnet_ids        = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
 
   tags = {
     Name      = "jung-ssm-endpoint"
@@ -11,10 +12,11 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id       = aws_vpc.jung-main.id
-  service_name = "com.amazonaws.ap-southeast-1.ec2messages"
-  subnet_ids   = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
+  vpc_id            = aws_vpc.jung-main.id
+  service_name      = "com.amazonaws.ap-southeast-1.ec2messages"
+  vpc_endpoint_type = "Interface"
   security_group_ids = [aws_security_group.jung-sg.id]
+  subnet_ids        = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
 
   tags = {
     Name      = "jung-ec2messages-endpoint"
@@ -23,10 +25,11 @@ resource "aws_vpc_endpoint" "ec2messages" {
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id       = aws_vpc.jung-main.id
-  service_name = "com.amazonaws.ap-southeast-1.ssmmessages"
-  subnet_ids   = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
+  vpc_id            = aws_vpc.jung-main.id
+  service_name      = "com.amazonaws.ap-southeast-1.ssmmessages"
+  vpc_endpoint_type = "Interface"
   security_group_ids = [aws_security_group.jung-sg.id]
+  subnet_ids        = [aws_subnet.jung-subnet-a.id, aws_subnet.jung-subnet-b.id]
 
   tags = {
     Name      = "jung-ssmmessages-endpoint"
