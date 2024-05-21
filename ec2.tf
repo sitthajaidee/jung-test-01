b1417f5dea1dc8d9d4dc83f0e38c8db7cf03afc0
Sittha Jaidee <sitthaja@ais.co.th>
2024-05-21 10:53:00 +0700

    update ec2 install mongodb shell

--- ec2.tf
+++ ec2.tf
@@ -11,6 +11,19 @@ resource "aws_instance" "jung-ec2" {
               yum install -y amazon-ssm-agent
               systemctl enable amazon-ssm-agent
               systemctl start amazon-ssm-agent
+
+              # Add the MongoDB repository
+              tee /etc/yum.repos.d/mongodb-org-6.0.repo <<EOL
+              [mongodb-org-6.0]
+              name=MongoDB Repository
+              baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+              gpgcheck=1
+              enabled=1
+              gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc
+              EOL
+
+              # Install mongodb-org-shell
+              yum install -y mongodb-org-shell
               EOF
 
   tags = {
